0362393fe260e039c7cfa618b5e7586d01c6f99f
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-31 01:53:20 +0900

    i can draw lines now

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from window import Window
+from point import Point, Line
 
 def main():
     win = Window(800, 600)
+
+    line = Line(Point(80,80), Point(160, 160))
+    win.draw_line(line, 'black')
+
     win.wait_for_close()
+
 
 if __name__ == '__main__':
     main()
